9d26529141ec4eed8a3d8d9ff5979a4c435be4b4
lxlonlyn <1124646822@qq.com>
2026-09-01 08:39:22 +0000

    bug fixed

--- scripts/agent_client.py
+++ scripts/agent_client.py
@@ -14,6 +14,9 @@ from typing import Any
 
 import httpx
 
+REPOSITORY_ROOT = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(REPOSITORY_ROOT))
+
 from paperos_core.config import RuntimeSettings, load_settings
 from paperos_core.errors import PaperOSError, public_diagnostic
 
